405a76af7204dd62730c3c3e15ca0c80ce24485d
vlad4400 <sleepvs@gmail.com>
2021-12-21 16:58:43 +0100

    deploy the project locally

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,11 @@
-const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+
+// webpack.mix.js
+
+const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .react()
-    .sass('resources/sass/app.scss', 'public/css');
+    .extract(['react'])
+    .postCss('resources/css/app.css', 'public/css')
+    .sourceMaps();
