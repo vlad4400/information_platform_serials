b60c7ca7f84abe61bded2eabd5f20f1e7eaf197e
Kseniia <kseniia.pomortseva@gmail.com>
2021-12-22 20:49:35 +0500

    api User-route fixed

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\Admin\UserController as AdminUserController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\UserController as AdminUserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function()
 {
-    Route::apiResource('/users', AdminUserController::class);
+    Route::get('/users', [AdminUserController::class, 'index']);
 });
-
