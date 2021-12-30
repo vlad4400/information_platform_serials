3a447a04ea7241ad9460e2d95d228490be2c922c
Bor333 <bsh91@mail.ru>
2021-12-30 22:17:29 +0700

    fix route CategoryController

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
 
-use App\Http\Controllers\CategoryController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\SerialController;
 use App\Http\Controllers\Api\Admin\SerialController as AdminSerialController;
 use App\Http\Controllers\Admin\UserController as AdminUserController;
