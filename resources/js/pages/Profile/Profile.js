7fc43bef677f66ff59b59ee321893b0d8bf5b481
Alexander Ignatev <alexign473@gmail.com>
2022-01-08 13:44:40 +0300

    add private route, profile settings page

--- resources/js/pages/Profile/Profile.js
+++ resources/js/pages/Profile/Profile.js
@@ -1,20 +1,14 @@
-import { Link, Outlet, Navigate } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 
 export const Profile = () => {
-  const user = true;
-  if (user) {
-    //если авторизован
-    return (
-      <div>
-        <h1>Профиль</h1>
-        <p>Здесь будет профиль пользователя и избранное</p>
-        <Link to={ROUTES.FAVOURITES}>Избранное</Link>
-        <Outlet />
-      </div>
-    );
-  } else {
-    return <Navigate to='/' />;
-    //либо перейти на страницу авторизации/регистрации
-  }
+  return (
+    <div>
+      <h1>Профиль</h1>
+      <p>Здесь будет профиль пользователя и избранное</p>
+      <Link to={ROUTES.FAVOURITES}>Избранное</Link>
+      <Link to={ROUTES.SETTINGS}>Настройки</Link>
+      <Outlet />
+    </div>
+  );
 };
