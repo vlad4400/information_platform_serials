8a8d5c7fd08c25ecd5c5273436165ec59f0792c0
Kseniia <43981905+PomorKse@users.noreply.github.com>
2022-01-12 21:16:02 +0500

    Home controller (#85)
    * fix composer.lock
    
    * lock delete, composer install
    
    * worker in procfile
    
    * document_root
    
    * 755
    
    * no posters
    
    * Storage&filesystems
    
    * images
    
    * no images
    
    * app view in routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 
 
-
-Route::get('/{any}', HomeController::class)
+Route::view('/{any}', 'app')
     ->where('any', '.*');
 
 
