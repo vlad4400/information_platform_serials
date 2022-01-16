d775b0a99a711b038f549644b5453ffd7e5fa1b5
Kseniia <43981905+PomorKse@users.noreply.github.com>
2022-01-16 21:25:14 +0500

    Registration (#105)
    * composer.lock
    
    * signup axios post
    
    * authController&&api/register route&&handling

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Routing } from './routing/Routing';
 import Navigation from './components/Navigation';
 import { getSerials } from './store/serials.slice';
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://127.0.0.1:8000";//prefixPAth;
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
+
+
+axios.defaults.withCredentials = true;
+
 
 export default function App() {
   const dispatch = useDispatch();
