22b94fe49e507a4d33c5600554133ed767509d62
Bor333 <79305611+Bor333@users.noreply.github.com>
2022-01-16 23:28:11 +0700

    Sort (#106)
    * add sort by year and by rate
    
    * move sort to Serial.php model
    
    * move sort to Serial.php model

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\SortController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,6 +25,8 @@ Route::apiResources(['serials' => SerialController::class]);
 Route::apiResources(['genres' => GenreController::class]);
 Route::post('serials/{serial}/favorite' , [SerialController::class, 'favorite']);
 Route::get('search', [SearchController::class, 'search']);
+Route::get('sortBy/year{order?}', [SortController::class, 'Year']);
+Route::get('sortBy/rate{order?}', [SortController::class, 'Rate']);
 
 Route::group(['prefix' => 'profile'], function()
 {
