51f27b7d6cf6af8f965ae76fe10f26fd01c917cc
Kseniia <kseniia.pomortseva@gmail.com>
2022-01-21 11:57:58 +0500

    logout_route&method

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,6 +13,12 @@ axios.defaults.headers.post['Accept'] = 'application/json';
 
 axios.defaults.withCredentials = true;
 
+axios.interceptors.request.use(function (config){
+  const token = localStorage.getItem('auth_token');
+  config.headers.Authorisation = token ? `Bearer ${token}` : '';
+  return config;
+})
+
 
 export default function App() {
   const dispatch = useDispatch();
