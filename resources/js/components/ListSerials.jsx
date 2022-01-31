114cb0a0caa39324520e1364eb4d5eeff33ac18d
vlad4400 <35049484+vlad4400@users.noreply.github.com>
2022-01-31 18:13:16 +0100

    Improve list serials (#139)
    * dev-server [with proxy]
    
    * fix 'path'
    
    * update 'serials' [add to/remove from favorites & sort]
    
    * add btn 'home'
    
    * update 'registration' [redirect to favorites]
    
    * fix serial service
    
    * update 'backend routes'
    
    * update 'serials.slice' [improve]
    
    * update 'serials.slice' [improve]
    
    * fix 'favorites'
    
    Co-authored-by: Kseniia <43981905+PomorKse@users.noreply.github.com>

--- resources/js/components/ListSerials.jsx
+++ resources/js/components/ListSerials.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import { ButtonGroup, Dropdown, DropdownButton, Figure, Spinner } from 'react-bootstrap';
+import { Figure, Spinner } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
+import { switchFavorite } from '../services/SerialsService';
+import { setFavoritesFailure, setLoading as setLoadingFavorites, setLoadingComplete as setLoadingCompleteFavorites } from '../store/favorites.slice';
+import { switchSerialIsFavoriteById } from '../store/serials.slice';
 
-export default ({title, serials, loading, showNavigation = false, isAuth = false}) => {
+export default ({title, serials, loading, isAuth = false}) => {
+
+    const dispatch = useDispatch();
+    
+    const onClickAddToFavorite = (id) => {
+        dispatch(setLoadingFavorites());
+        dispatch(switchSerialIsFavoriteById(id));
+
+        switchFavorite(id).then()
+        .catch(() => {
+            dispatch(switchSerialIsFavoriteById(id));
+            dispatch(setFavoritesFailure());
+        })
+        .finally(() => {
+            dispatch(setLoadingCompleteFavorites());
+        });
+    }
     
     const Item = ({ri, serial }) => (
         <div style={{display: 'flex', margin: '0 5px' }}>
@@ -35,8 +54,9 @@ export default ({title, serials, loading, showNavigation = false, isAuth = false
                         variant="outline-success"
                         size={'sm'}
                         style={{marginRight: '10px'}}
-                        className={'btn-add-plus'}
-                    >+</Button>
+                        className={serial.isFavorite ? 'btn-remove-minus' : 'btn-add-plus'}
+                        onClick={() => onClickAddToFavorite(serial.id)}
+                    >{serial.isFavorite ? '-' : '+'}</Button>
                     : <></>
                 }
                 <span style={{
@@ -53,34 +73,13 @@ export default ({title, serials, loading, showNavigation = false, isAuth = false
         serials.map((serial, ri) => <Item key={serial.id} ri={ri} serial={serial} />)
     );
 
+
     return (
         <div className="top-rating" style={{marginTop: '15px'}}>
             <h2>{title}</h2>
 
             { !loading
                 ? <>
-                    { showNavigation
-                        ? <div style={{
-                            display: 'flex',
-                            justifyContent: 'space-between',
-                            marginBottom: '15px',
-                            }}>
-                            <DropdownButton title="Отобразить" id="bg-nested-dropdown">
-                                <Dropdown.Item eventKey="1">От найбольшего оличества просмотров</Dropdown.Item>
-                                <Dropdown.Item eventKey="2">От найменьшего оличества просмотров</Dropdown.Item>
-                                <Dropdown.Item eventKey="3">От найбольшего рейтинга</Dropdown.Item>
-                                <Dropdown.Item eventKey="3">От найменьшего рейтинга</Dropdown.Item>
-                            </DropdownButton>
-                            <ButtonGroup className="me-2" aria-label="First group">
-                                <Button variant="secondary">1</Button>{' '}
-                                <Button variant="secondary">2</Button>{' '}
-                                <Button variant="secondary">3</Button>{' '}
-                                <Button variant="secondary">4</Button>{' '}
-                                <Button variant="secondary">5</Button>
-                            </ButtonGroup>
-                        </div>
-                        : <></>
-                    }
                     <List serials={serials}/>
                 </>
                 : <div style={{display: 'flex', justifyContent: 'center'}}>
--- resources/js/constants/api.js
+++ resources/js/constants/api.js
@@ -1,12 +1,12 @@
 //константы
-const prefixPAth = 'https://boiling-anchorage-83800.herokuapp.com/api';
-export const API_ADMIN_USERS = prefixPAth + '/admin/users';  //все пользователи
-export const API_SERIALS = prefixPAth + '/serials';      //все сериалы
+const prefixPAth = '';
+export const API_ADMIN_USERS = prefixPAth + '/api/admin/users';  //все пользователи
+export const API_SERIALS = prefixPAth + '/api/serials';      //все сериалы
 // /api/serials/{id}-сериал с id={id}
 export const API_ADMIN_SERIALS = prefixPAth + '/admin/serials';    //все сериалы для админа(resourse)
 // /api/admin/serials/{id}-сериал с id={id} для админа(для изменения, добавления и удаления)
 export const API_ADMIN_PARSER = prefixPAth + '/admin/parser';     //для админа(парсит сериалы)
 
 export const API_REG_COOKIE = '/sanctum/csrf-cookie';
-export const API_REGISTER = '/register';
-export const API_FAVORITES = '/favorites'
+export const API_REGISTER = '/api/register';
+export const API_FAVORITES = '/api/favorites';
