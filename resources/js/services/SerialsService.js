7683b66c9a59335be44c44f5bbd348be4fc5a3e2
Kseniia <43981905+PomorKse@users.noreply.github.com>
2022-02-01 01:38:41 +0500

    Sort controller fetch data (#145)
    * sortController&FetchData
    
    * sortController&FetchData

--- resources/js/services/SerialsService.js
+++ resources/js/services/SerialsService.js
@@ -4,17 +4,17 @@ export const switchFavorite = (id) => {
     return authAxios.put(`/favorites/${id}`);
 }
 
-export const getSerialsByRate = (first, pageNumber, sortOrder = 'ASC') => {
-    // return authAxios.get(`/serials`); //for tests
-    return authAxios.get(`/search/rate/${first}/${pageNumber}/${sortOrder == 'ASC' ? '0' : '1'}`);
+export const getSerialsByRate = (rate, pageNumber, sortOrder = 'ASC') => {
+    //return authAxios.get(`/serials`); //for tests
+    return authAxios.get(`/search/rate/${rate}/${pageNumber}/${sortOrder == 'ASC' ? '1' : '0'}`);
 }
 
-export const getSerialsByYear = (first, pageNumber) => {
-    return authAxios.get(`/search/year/${first}/${pageNumber}`);
+export const getSerialsByYear = (year, pageNumber) => {
+    return authAxios.get(`/search/year/${year}/${pageNumber}`);
 }
 
 export const getSerialsByRateTop50Asc = () => {
-    return authAxios.get(`/search/rate/1/50/0`);
+    return authAxios.get(`/search/rate/10/50/0`);
 }
 
 export const getSerialsByRateTop50Desc = () => {
@@ -22,9 +22,9 @@ export const getSerialsByRateTop50Desc = () => {
 }
 
 export const getSerialsByYearTop50Asc = () => {
-    return authAxios.get(`/search/year/1/50/0`);
+    return authAxios.get(`/search/year/1900/50/1`);
 }
 
 export const getSerialsByYearTop50Desc = () => {
-    return authAxios.get(`/search/year/1/50/1`);
+    return authAxios.get(`/search/year/2022/50/0`);
 }
