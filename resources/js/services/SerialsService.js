4c499bc0fbc609e92e3bac49b34cceece3c663c6
vlad4400 <35049484+vlad4400@users.noreply.github.com>
2022-02-01 12:20:28 +0100

    fix (#147)

--- resources/js/services/SerialsService.js
+++ resources/js/services/SerialsService.js
@@ -14,17 +14,17 @@ export const getSerialsByYear = (year, pageNumber) => {
 }
 
 export const getSerialsByRateTop50Asc = () => {
-    return authAxios.get(`/search/rate/10/50/0`);
+    return authAxios.get(`/search/rate/0/50`);
 }
 
 export const getSerialsByRateTop50Desc = () => {
-    return authAxios.get(`/search/rate/1/50/1`);
+    return authAxios.get(`/search/rate/10/50/1`);
 }
 
 export const getSerialsByYearTop50Asc = () => {
-    return authAxios.get(`/search/year/1900/50/1`);
+    return authAxios.get(`/search/year/0/50`);
 }
 
 export const getSerialsByYearTop50Desc = () => {
-    return authAxios.get(`/search/year/2022/50/0`);
+    return authAxios.get(`/search/year/2030/50/1`);
 }
